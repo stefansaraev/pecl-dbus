ad5f69cb570817cd46ab035db8f36eb4a1f83257
Derick Rethans <github@derickrethans.nl>
2009-03-07 22:33:20 +0000

    - Added some data to my signal sending example.

--- examples/send-signal.php
+++ examples/send-signal.php
@@ -2,5 +2,5 @@
 $d = new Dbus( Dbus::BUS_SESSION, true );
 $d->requestName( 'nl.derickrethans.test' );
 $s = new DbusSignal( $d, '/nl/derickrethans/SignalObject', 'nl.derickrethans.Interface', 'Test' );
-$s->send();
+$s->send( "ze data" );
 ?>
